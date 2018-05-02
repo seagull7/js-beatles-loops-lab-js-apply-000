c323b97b7eb8e5573e3f5f78e7d7f7bf0566a4fe
seagull7 <samsiegel527@gmail.com>
2018-05-02 21:26:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 
-theBeatlesPlay(musicians, instruments); 
+theBeatlesPlay(musicians, instruments);
 
 function theBeatlesPlay(musicians, instruments) {
-  var newArr = []; 
+  var newArr = [];
   for(var i =0; i < musicians.length; i++) {
     newArr.push(musicians[i] + " plays " + instruments[i]);
   }
   return newArr;
 }
-
